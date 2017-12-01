e69317033ebc4d064dd36a28c2e5ca103da6993b
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2017-12-01 17:55:22 +0100

    Added some config for Git and editors

---
+++ Day 2/part2.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+checksum = 0
+File.open('part1_input.txt').each do |line_raw|
+  numbers_on_line = []
+  line_raw.delete("\n").split("\t").each do |number|
+    numbers_on_line << number.to_i
+  end
+  checksum += numbers_on_line.max - numbers_on_line.min
+end
+puts checksum
