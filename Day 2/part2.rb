2571602035a5f2f4b7cdfde717c906e54c74cacc
TheWrongAlice <alice.cyan.carlsson@gmail.com>
2017-12-03 11:23:19 +0100

    Day 2

--- Day 2/part2.rb
+++ Day 2/part2.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 
 checksum = 0
-File.open('part1_input.txt').each do |line_raw|
+File.open('input.txt').each do |line_raw|
   numbers_on_line = []
   line_raw.delete("\n").split("\t").each do |number|
     numbers_on_line << number.to_i
   end
-  checksum += numbers_on_line.max - numbers_on_line.min
+
+  is_even = false
+  numbers_on_line.each_with_index do |a, index_a|
+    numbers_on_line.each_with_index do |b, index_b|
+      next if index_a == index_b # Don't divide by itself
+
+      is_even = (a.to_f % b.to_f).zero?
+
+      if is_even
+        checksum += (a / b)
+        break
+      end
+    end
+    break if is_even
+  end
+
 end
 puts checksum
